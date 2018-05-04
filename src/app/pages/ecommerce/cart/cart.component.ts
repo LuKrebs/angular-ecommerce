1d9f5ad2adc0e38aaaad55b8c246d7c5b7b73eec
lucianokrebs <lkrebs05@gmail.com>
2018-05-03 22:18:43 -0300

    creating cart page with its own logic

--- src/app/pages/ecommerce/cart/cart.component.ts
+++ src/app/pages/ecommerce/cart/cart.component.ts
@@ -7,16 +7,19 @@ import { ProductsService } from '../../../services/products.service';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-
   cartAsString;
   cartAsArray;
   cartAsObject: any;
   cartProducts: any[];
+  defaultAltImage = "Sonja Baby's | A beleza está nos detalhes";
+  purchaseDetails;
+  purchaseTotalAmount = 0;
 
   constructor(
     private productService: ProductsService,
   ) { 
     this.cartAsObject = {};
+    this.purchaseDetails = {};
     this.cartProducts = [];
     this.cartAsString = localStorage.getItem('cart');
     this.cartAsArray = this.cartAsString.split('?');
@@ -30,16 +33,30 @@ export class CartComponent implements OnInit {
 
     Object.keys(this.cartAsObject).forEach(productId => {
       this.productService.read(productId).then((product: any) => {
-        product.cartQty = this.cartAsObject[productId];
+        product.cartQty = parseInt(this.cartAsObject[productId]);
+        product.showMaxQtyMessage = false;
+        this.purchaseTotalAmount += (parseInt(product.cartQty) * product.price);
         this.cartProducts.push(product);
       });
-
     });
     
-    console.log(this.cartProducts);
   }
 
   ngOnInit() {
+  }
+
+  addQty(product) {
+    if (product.cartQty < product.qty) return product.cartQty += 1;
+    else return product.showMaxQtyMessage = true;
+  }
+
+  removeQty(product) {
+    if (product.cartQty == 0) return product.cartQty = 0;
+    return product.cartQty -= 1;
+  }
+
+  getFormattedValue(value: number) {
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
   }
 
 }
